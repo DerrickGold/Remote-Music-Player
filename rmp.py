455280c33743588e202695fce9bb1444d94263c1
Derrick <Derrick.b.gold@gmail.com>
2016-08-20 14:38:18 -0700

    Scans directory from first argument given

--- rmp.py
+++ rmp.py
@@ -1,15 +1,44 @@
+#!/usr/bin/python3
+
 from flask import Flask
 import os
+import sys
+
 app = Flask(__name__)
 
+MUSIC_LIST_NAME = ".music"
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    for root, dirs, files in os.walk('/tmp'):
-        print(dirs, files)
 
 
+def generateMusicList(musicRoot, outputFile):
+
+    try:
+        listFile = open(outputFile, "w")
+    except (e):
+        print(e)
+        return
+
+    count = 0;
+    
+    for root, dirs, files in os.walk(musicRoot):
+        for f in files:
+            count+=1
+            if count % 1000 == 0:
+                print("Generating music list... {}".format(count))
+            
+            listFile.write(os.path.join(root, f).replace(musicRoot, '') + '\n')
+
+    print("Scanned {} files.".format(count))
+    
+def main():
+    musicDir = sys.argv[1]
+    generateMusicList(musicDir, MUSIC_LIST_NAME)
     app.run()
+
+
+if __name__ == "__main__":
+    main()
