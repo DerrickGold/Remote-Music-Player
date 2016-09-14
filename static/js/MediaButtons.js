10fb3377040dab237459991a8f6029d684c6f7ad
Thomas <id@somebananas.com>
2016-09-14 01:55:10 +0000

    Fix.

--- static/js/MediaButtons.js
+++ static/js/MediaButtons.js
@@ -11,9 +11,9 @@ MediaButtons = function(evtSys, mediaLibrary) {
   var updatePlayPauseBtn = function(newState) {
     self.currentState = newState;
     var icon = document.getElementById("media-btn-play-icon");
-    var isntPlaying = isntPlaying(newState);
-    icon.classList.toggle('glyphicon-pause', isntPlaying);
-    icon.classList.toggle('glyphicon-play', !isntPlaying);
+    var notPlaying = isntPlaying(newState);
+    icon.classList.toggle('glyphicon-pause', notPlaying);
+    icon.classList.toggle('glyphicon-play', !notPlaying);
   }
 
   var playPauseBtn = document.getElementById("media-btn-play");
